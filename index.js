8ada9101bd1a841f2444b437ab98da8eb4817d65
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-11-27 16:45:19 +0500

    the end

---
+++ index.js
@@ -0,0 +1,113 @@
+// // assignment operators - tayinlash operatori
+
+// let a = 5 
+
+// // =
+// // -
+// // +=
+// // *=
+// // /=
+// // %=
+// // **=
+
+// a **= 3; //a = a ** 3;
+
+// console.log(a);
+
+// tarmoqlanuvchi operator
+
+
+
+// 1-misol
+// let a = +prompt("Birinchi insoning yoshini kiriting")
+// let b = +prompt("Ikkinchi insoning yoshini kiriting")
+
+// if (a > b) {
+//     console.log(a ** 2);
+// } else {
+//     console.log(b ** 2);
+// }
+
+// 2-misol
+// let a = +prompt("Birinchi insoning yoshini kiriting")
+// let b = +prompt("Ikkinchi insoning yoshini kiriting")
+// let c = +prompt("Uchinchi insoning yoshini kiriting")
+
+// if (a > b) {
+//     console.log(a);
+// }
+// if (c > b) {
+//     console.log(c);
+// }else{
+//     console.log(b);
+// }
+
+// 3-misol
+
+// let a = +prompt("Birinchi insoning yoshini kiriting")
+
+// if (a % 2 == 0) {
+//     console.log("juft son");
+// }else{
+//     console.log("toq son");
+// }
+
+
+// 4 - misol
+
+// let a = +prompt("Birinchi insoning yoshini kiriting")
+
+// if (a % 5 == 0) {
+//     console.log("rost");
+// }else{
+//     console.log("yolg'on");
+// }
+
+
+// 5 - misol ?
+
+// let a = +prompt("Birinchi insoning yoshini kiriting");
+
+// let b = a % 10
+// let c = b % 10
+// let d = c / 10
+
+// console.log(d);
+
+
+
+// 6 - misol
+
+// let a = +prompt("Birinchi insoning yoshini kiriting");
+
+// let b = a % 10
+
+// console.log(c);
+
+
+// 7 - misol ?
+
+
+
+
+//  8 - misol
+
+// let a = +prompt("Birinchi insoning yoshini kiriting");
+
+// if (a % 10 == 2) {
+//     console.log("rost");
+// }else{
+//     console.log("yolg'on");
+// }
+
+
+// 9 - misol
+
+// let a = +prompt("Birinchi insoning yoshini kiriting");
+
+// let b = a / 10
+// let c = a % 10
+
+// console.log(c);
+
+// 10 - misol
